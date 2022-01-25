4be8867528bfababb0b65e3c72c3d25c6f677bb3
anushka-2001 <anushka2001feb@gmail.com>
2022-01-25 15:50:50 +0530

    second commit

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.fields import DateTimeField
 from django.urls import reverse
+# from 
+import numpy
 from django.utils import timezone
 
 # Create your models here.
